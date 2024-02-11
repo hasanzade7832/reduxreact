4d81d2293c31a2d17b0f369ccb9519e564b99835
hasanzade7832 <hasanzade.hoseyn27@gmail.com>
2024-02-11 18:52:33 +0330

    work on ribbon

--- src/components/Roles/Add-Edit/Roles.jsx
+++ src/components/Roles/Add-Edit/Roles.jsx
@@ -5,7 +5,9 @@ import { fetchRoles } from "../../../redux/roles/rolesSlice";
 import AddBar from "../../globalComponents/main/addBar";
 import EditBar from "../../globalComponents/main/editBar";
 import { Toast } from "primereact/toast";
+import { InputSwitch } from "primereact/inputswitch";
 import CustomInputText from "../../globalComponents/main/inputCom";
+import CustomTextArea from "../../globalComponents/main/inputTextAreaComp";
 import projectServices from "../../services/project.services";
 
 const RolesAdd = () => {
@@ -43,12 +45,28 @@ const RolesAdd = () => {
       setRolesData((prevFormData) => ({
         ...prevFormData,
         Name: "",
+        PostCode: "",
+        Description: "",
+        Responsibility: "",
+        Authorization: "",
+        Competencies: "",
+        Grade: "",
+        Type: "",
+        isStaticPost: false,
       }));
     } else if (selectedRow) {
       dispatch(mainSlice.actions.setIsEditMode(true));
       setRolesData((prevFormData) => ({
         ...prevFormData,
         Name: selectedRow.Name,
+        PostCode: selectedRow.PostCode,
+        Description: selectedRow.Description,
+        Responsibility: selectedRow.Responsibility,
+        Authorization: selectedRow.Authorization,
+        Competencies: selectedRow.Competencies,
+        Grade: selectedRow.Grade,
+        Type: selectedRow.Type,
+        isStaticPost: selectedRow.isStaticPost,
       }));
     }
   }, [isAddClicked, selectedRow]);
@@ -57,6 +75,14 @@ const RolesAdd = () => {
     setRolesData((prevFormData) => ({
       ...prevFormData,
       [fieldName]: value,
+    }));
+  };
+
+  const handleStaticPostChange = (e) => {
+    console.log("eeeeee", e.value);
+    setRolesData((prevFormData) => ({
+      ...prevFormData,
+      isStaticPost: e.value,
     }));
   };
 
@@ -73,6 +99,14 @@ const RolesAdd = () => {
         setRolesData((prevFormData) => ({
           ...prevFormData,
           Name: "",
+          PostCode: "",
+          Description: "",
+          Responsibility: "",
+          Authorization: "",
+          Competencies: "",
+          Grade: "",
+          Type: "",
+          isStaticPost: false,
         }));
       })
       .catch(() => {});
@@ -82,6 +116,14 @@ const RolesAdd = () => {
     const updatedSelectedRow = {
       ...selectedRow,
       Name: rolesData.Name,
+      PostCode: rolesData.PostCode,
+      Description: rolesData.Description,
+      Responsibility: rolesData.Responsibility,
+      Authorization: rolesData.Authorization,
+      Competencies: rolesData.Competencies,
+      Grade: rolesData.Grade,
+      Type: rolesData.Type,
+      isStaticPost: rolesData.isStaticPost,
     };
     projectServices
       .updatePost(updatedSelectedRow)
@@ -92,9 +134,17 @@ const RolesAdd = () => {
           summary: "Success",
           detail: "Item Edited successfully",
         });
-        rolesData((prevFormData) => ({
+        setRolesData((prevFormData) => ({
           ...prevFormData,
           Name: "",
+          PostCode: "",
+          Description: "",
+          Responsibility: "",
+          Authorization: "",
+          Competencies: "",
+          Grade: "",
+          Type: "",
+          isStaticPost: false,
         }));
       })
       .catch(() => {});
@@ -119,6 +169,7 @@ const RolesAdd = () => {
             maxHeight: "calc(100vh - 12rem)",
           }}
         >
+          {/* /////////////////////////Line 1///////////////////////////////////// */}
           <div
             className="grid"
             style={{ marginLeft: "20px", marginTop: "20px" }}
@@ -126,7 +177,7 @@ const RolesAdd = () => {
             <div className="col-5">
               <div>
                 <CustomInputText
-                  label="Name"
+                  label="Role"
                   value={rolesData.Name}
                   onChange={(e) => handleChange("Name", e.target.value)}
                 />
@@ -134,7 +185,104 @@ const RolesAdd = () => {
             </div>
             <div className="col-1"></div>
             <div className="col-5">
-              {/* <CustomInputText label="ID" disabled={"disabled"} /> */}
+              <div>
+                <CustomInputText
+                  label="Role Code"
+                  value={rolesData.PostCode}
+                  onChange={(e) => handleChange("PostCode", e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+          {/* /////////////////////////Line 2///////////////////////////////////// */}
+          <div
+            className="grid"
+            style={{ marginLeft: "20px", marginTop: "20px" }}
+          >
+            <div className="col-5">
+              <div>
+                <CustomTextArea
+                  label="Job Description"
+                  value={rolesData.Description}
+                  onChange={(e) => handleChange("Description", e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="col-1"></div>
+            <div className="col-5">
+              <CustomTextArea
+                label="Responsibilities"
+                value={rolesData.Responsibility}
+                onChange={(e) => handleChange("Responsibility", e.target.value)}
+              />
+            </div>
+          </div>
+          {/* /////////////////////////Line 3///////////////////////////////////// */}
+          <div
+            className="grid"
+            style={{ marginLeft: "20px", marginTop: "20px" }}
+          >
+            <div className="col-5">
+              <div>
+                <CustomTextArea
+                  label="Authorities"
+                  value={rolesData.Authorization}
+                  onChange={(e) =>
+                    handleChange("Authorization", e.target.value)
+                  }
+                />
+              </div>
+            </div>
+            <div className="col-1"></div>
+            <div className="col-5">
+              <CustomTextArea
+                label="Competencies"
+                value={rolesData.Competencies}
+                onChange={(e) => handleChange("Competencies", e.target.value)}
+              />
+            </div>
+          </div>
+          {/* /////////////////////////Line 4///////////////////////////////////// */}
+          <div
+            className="grid"
+            style={{ marginLeft: "20px", marginTop: "20px" }}
+          >
+            <div className="col-5">
+              <div>
+                <CustomInputText
+                  label="Grade"
+                  value={rolesData.Grade}
+                  onChange={(e) => handleChange("Grade", e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="col-1"></div>
+            <div className="col-5">
+              <CustomInputText
+                label="Type"
+                value={rolesData.Type}
+                onChange={(e) => handleChange("Type", e.target.value)}
+              />
+            </div>
+          </div>
+          {/* /////////////////////////Line 4///////////////////////////////////// */}
+          <div
+            className="grid"
+            style={{ marginLeft: "20px", marginTop: "20px" }}
+          >
+            <div className="col-5">
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <InputSwitch
+                  id="staticRole"
+                  value={rolesData.isStaticPost}
+                  checked={rolesData.isStaticPost}
+                  onChange={handleStaticPostChange}
+                  size="small"
+                />
+                <label style={{ marginLeft: "5px" }} htmlFor="staticRole">
+                  Static Role
+                </label>
+              </div>
             </div>
           </div>
         </div>
