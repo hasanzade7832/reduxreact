b2d996d43e1b1c5332424924d6327c6ecb1d7fc1
Ryan-PG <heidarih313@gmail.com>
2024-02-17 18:09:33 +0330

    work on assignment

--- src/components/assignments/tableBox/tableBoxRole.jsx
+++ src/components/assignments/tableBox/tableBoxRole.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import "../styles/tableStyles.css"
+import { useSelector, useDispatch } from "react-redux";
 
 export default function TableBoxRole() {
   const dataTableValues = [
@@ -11,10 +12,13 @@ export default function TableBoxRole() {
     { id: 4, Name: 'Michael Clark'}
   ];
 
+  const dataAssignments = useSelector((state) => state.dataAssignment.dataAssignment);
+  console.log("dataAssignments",dataAssignments)
+
   return (
     <div className="table-container">
       <h2>Employee Information</h2>
-      <DataTable value={dataTableValues}>
+      <DataTable value={dataAssignments}>
         <Column field="Name" header="Name"></Column>
       </DataTable>
     </div>
