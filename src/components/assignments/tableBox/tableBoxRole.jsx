0635d487828a4009f94551ee777b2d6924aef538
Ryan-PG <heidarih313@gmail.com>
2024-02-17 17:52:31 +0330

    work on assignment

--- src/components/assignments/tableBox/tableBoxRole.jsx
+++ src/components/assignments/tableBox/tableBoxRole.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import "../styles/tableStyles.css"
 
-export default function tableBoxRole() {
+export default function TableBoxRole() {
+  const dataTableValues = [
+    { id: 1, Name: 'John Doe' },
+    { id: 2, Name: 'Jane Smith' },
+    { id: 3, Name: 'Mary Johnson'},
+    { id: 4, Name: 'Michael Clark'}
+  ];
+
   return (
-    <div>tableBoxRole</div>
-  )
+    <div className="table-container">
+      <h2>Employee Information</h2>
+      <DataTable value={dataTableValues}>
+        <Column field="Name" header="Name"></Column>
+      </DataTable>
+    </div>
+  );
 }
